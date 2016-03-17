91143fc87bfd813e9e03d75da81042514d86af04
logiclogue <jordanpaullord@hotmail.co.uk>
2016-03-17 22:01:56 +0000

    Update Dijstra class

--- build/all.js
+++ build/all.js
@@ -8,7 +8,23 @@ class Canvas {
 		this.height = this.c.height;
 	}
 }
-class Dijkstra {}
+class Dijkstra {
+	selectGraph(graph) {
+		graph.nodes.forEach(function (node) {
+			node.dijkstra = this.obj;
+		});
+	}
+
+	run() {}
+
+	constructor() {
+		this.obj = function () {
+			this.permanentLabel;
+			this.temporaryLabel;
+			this.smallestValue;
+		};
+	}
+}
 class Graph {
 	addNode(name, edges) {
 		this.nodes[name] = {
--- js/Dijkstra.js
+++ js/Dijkstra.js
@@ -1,4 +1,21 @@
 class Dijkstra
 {
-	
+	selectGraph(graph) {
+		graph.nodes.forEach(function (node) {
+			node.dijkstra = this.obj;
+		});
+	}
+
+	run() {
+
+	}
+
+
+	constructor() {
+		this.obj = function () {
+			this.permanentLabel; 
+			this.temporaryLabel;
+			this.smallestValue;
+		};
+	}
 }
