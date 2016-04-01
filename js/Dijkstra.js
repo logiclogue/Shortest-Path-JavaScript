204e5801106e2fc00314861b0008233883d28a71
logiclogue <jordanpaullord@hotmail.co.uk>
2016-04-01 18:21:07 +0100

    Update dijkstra's algorithm, massive progress

--- js/Dijkstra.js
+++ js/Dijkstra.js
@@ -4,10 +4,15 @@ class Dijkstra
 		var lowest;
 
 		this.testing.forEach(function (node) {
-			node.edges.forEach(function (edge) {
-				
-			});
+			lowest = lowest || node;
+
+			if (node.working.shortestDistance < lowest.working.shortestDistance) {
+				lowest = node;
+			}
 		});
+
+		this.testing.splice(this.testing.indexOf(lowest), 1);
+		this.complete.push(lowest);
 	}
 
 	_convertStringToNode(string) {
@@ -22,17 +27,56 @@ class Dijkstra
 		});
 	}
 
+	_addWorkingObj(nodes) {
+		var self = this;
+
+		nodes.forEach(function (node) {
+			node.working = new self.NodeObj();
+			node.working.shortestDistance = 0;
+		});
+	}
+
 	_findAdjacentNodes() {
 		var self = this;
 
 		this.complete.forEach(function (node) {
 			node.edges.forEach(function (edge) {
 				var nodeObj = self._convertStringToNode(edge.endNode);
+				var newDistance = node.working.shortestDistance + edge.val;
 
-				if (self.testing.indexOf(nodeObj) === -1) {
+				// Is not in testing array and not in complete array.
+				// In other words, if the node hasn't been seen by the algorithm yet.
+				if (self.testing.indexOf(nodeObj) === -1 && self.complete.indexOf(nodeObj) === -1) {
+					nodeObj.working = new self.NodeObj();
+					nodeObj.working.shortestDistance = newDistance;
+					nodeObj.working.previousNode = node;
+
 					self.testing.push(nodeObj);
 				}
+				// Found shorter distance
+				else if (nodeObj.working.shortestDistance > newDistance) {
+					nodeObj.working.shortestDistance = newDistance;
+					nodeObj.working.previousNode = node;
+				}
 			});
+		});
+	}
+
+	_drawNode(node, colour) {
+		var coords = node.theName.split(',');
+
+		Canvas.drawSquare(coords[0], coords[1], colour);
+	}
+
+	_draw() {
+		var self = this;
+
+		this.testing.forEach(function (node) {
+			self._drawNode(node, '#00FFFF');
+		});
+
+		this.complete.forEach(function (node) {
+			self._drawNode(node, '#0000FF');
 		});
 	}
 
@@ -42,12 +86,16 @@ class Dijkstra
 	}
 
 	run() {
+		var self = this;
 		this.complete = this.graph.startNodes.slice();
 
-		this._lowestVertex();
-		this._findAdjacentNodes();
+		this._addWorkingObj(this.complete);
 
-		console.log(this.testing);
+		setInterval(function () {
+			self._findAdjacentNodes();
+			self._lowestVertex();
+			self._draw();
+		}, 100);
 	}
 
 
@@ -56,10 +104,9 @@ class Dijkstra
 		this.testing = [];
 		this.graph = {};
 
-		this.obj = function () {
-			this.permanentLabel;
-			this.temporaryLabel;
-			this.smallestValue;
+		this.NodeObj = function () {
+			this.shortestDistance;
+			this.previousNode;
 		};
 
 		this.selectGraph(graph);
